a0b378856ddb6d19466f5241723578a27ca50aad
CC <christian@ejlskovdesign.dk>
2023-07-10 19:36:32 +0200

    glass

--- assets/base.js
+++ assets/base.js
@@ -60,11 +60,12 @@ class Slider extends HTMLElement {
       for (const card of cards) {
         if (inputSlider.id == "Blur") {
           card.style.backdropFilter = "blur(" + inputSlider.value + "px)";
-          card.style.webkitBackdropFilter = "blur(" + inputSlider.value + "px)";
+          card.style["-webkit-backdrop-filter"] =
+            "blur(" + inputSlider.value + "px)";
           elementBlur.innerHTML =
             "backdrop-filter: blur" + "(" + inputSlider.value + "px" + ");";
           eelementWebkitBlur.innerHTML =
-            "webkit-backdrop-filter: blur" +
+            "-webkit-backdrop-filter: blur" +
             "(" +
             inputSlider.value +
             "px" +
